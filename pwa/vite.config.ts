a935c0f4e302e3a985eb91ba5229fcf9e45dc986
pdrzewicki <pdrzewicki@divante.com>
2023-02-21 14:54:45 +0100

    (feat) Install husky for pre-commits

---
+++ pwa/vite.config.ts
@@ -0,0 +1,69 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+import ElementPlus from 'unplugin-element-plus/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+import { VitePWA } from "vite-plugin-pwa"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(),
+      ElementPlus({
+        importStyle: 'css',
+        useSource: true
+      }),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+  //   , VitePWA({
+  //   mode: "development",
+  //   base: "/",
+  //   srcDir: "src",
+  //   filename: "sw.ts",
+  //   includeAssets: ["/favicon.png"],
+  //   strategies: "injectManifest",
+  //   manifest: {
+  //     name: "Predicty",
+  //     short_name: "Predicty",
+  //     theme_color: "#ffffff",
+  //     start_url: "/",
+  //     display: "standalone",
+  //     background_color: "#ffffff",
+  //     icons: [
+  //       {
+  //         src: "icon-192.png",
+  //         sizes: "192x192",
+  //         type: "image/png",
+  //       },
+  //       {
+  //         src: "/icon-512.png",
+  //         sizes: "512x512",
+  //         type: "image/png",
+  //       },
+  //       {
+  //         src: "icon-512.png",
+  //         sizes: "512x512",
+  //         type: "image/png",
+  //         purpose: "any maskable",
+  //       },
+  //     ],
+  //   },
+  // })
+  ],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    ],
+  },
+})
