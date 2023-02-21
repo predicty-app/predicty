eca6f717dbcd006c457562f67d2a3b6af0169087
pdrzewicki <pdrzewicki@divante.com>
2023-02-21 15:54:44 +0100

    (feat) Install Storybook

---
+++ pwa/.storybook/main.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const url = require("url");
+const tsconfigPaths = require("vite-tsconfig-paths").default;
+
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@etchteam/storybook-addon-status",
+  ],
+  "framework": "@storybook/vue3",
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
+  "features": {
+    "storyStoreV7": true,
+    "interactionsDebugger": true,
+  },
+  viteFinal(config) {
+    return {
+      ...config,
+      resolve: {
+        alias: {
+          ...config.resolve.alias,
+          '@': path.resolve(__dirname, '../src'),
+          'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js'
+        }
+      },
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: `@import '../src/assets/scss/index.scss';`,
+          },
+        },
+      },
+      plugins: [...config.plugins, tsconfigPaths()],
+    };
+  },
+}
