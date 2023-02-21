eae5d9da2be48822e6b418e31c3045442b8abc1f
pdrzewicki <pdrzewicki@divante.com>
2023-02-21 15:43:06 +0100

    (feat) Install i18n plugin for translations

--- pwa/env.d.ts
+++ pwa/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+declare module 'uuid'
+declare module 'vue-i18n'
+
+interface ImportMetaEnv {
+
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
