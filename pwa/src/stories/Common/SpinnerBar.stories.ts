af39453a5b8ecad1e2475f1f64d9c7bbe812be17
pdrzewicki <pdrzewicki@divante.com>
2023-02-22 14:33:58 +0100

    (feat) Create new component SpinnerBar

---
+++ pwa/src/stories/Common/SpinnerBar.stories.ts
@@ -0,0 +1,39 @@
+import SpinnerBarComponent from '@/components/Common/SpinnerBar.vue'
+
+type ComponentPropsType = {}
+
+export default {
+  component: SpinnerBarComponent,
+  title: 'Common/SpinnerBar',
+  argTypes: {
+    isVisible: {
+      name: 'isVisible',
+      description: 'Param for toogle visible spinner.',
+      type: { name: 'boolean', required: true },
+    },
+  },
+  parameters: {
+    status: {
+      type: 'stable',
+    },
+    docs: {
+      description: {
+        component: 'Component for spinner bar',
+      },
+    },
+  },
+}
+
+export const SpinnerBar = (args: ComponentPropsType) => ({
+  components: { SpinnerBarComponent },
+  setup() {
+    return { args }
+  },
+  template: `
+    <SpinnerBarComponent v-bind="args" />
+  `,
+})
+
+SpinnerBar.args = {
+  isVisible: true
+}
