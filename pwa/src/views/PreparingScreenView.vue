0c2872c9794df2c52a82466f1ac25a8cbd4c1f22
pdrzewicki <125042105+pdrzewicki@users.noreply.github.com>
2023-02-23 14:05:02 +0100

    PRED-30 Preparing finalize screen view (#12)
    * (feat) Preapre Finalize screen view
    
    * (feat) Create Finalize screen view

--- pwa/src/views/PreparingScreenView.vue
+++ pwa/src/views/PreparingScreenView.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n'
 
+const { t } = useI18n()
 </script>
 
 <template>
-
+  <OnBoardingLayout>
+    <template #header>
+      <AppLogo />
+    </template>
+    <template #content>
+      <div class="text-center">
+        <HeaderText :header-title="t('views.preparing-screen.header-title')"
+          :header-description="t('views.preparing-screen.header-description')" />
+        <SpinnerBar :is-visible="true"/>
+      </div>
+    </template>
+  </OnBoardingLayout>
 </template>
