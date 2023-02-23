d6c0f1c68b88219fb247d4c134d0053816dc8e3c
pdrzewicki <pdrzewicki@divante.com>
2023-02-23 13:58:56 +0100

    (feat) Preapre Finalize screen view

--- pwa/src/views/PreparingScreenView.vue
+++ pwa/src/views/PreparingScreenView.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n'
 
+const { t } = useI18n()
 </script>
 
 <template>
-
+  <OnBoardingLayout>
+    <template #header>
+      <AppLogo />
+    </template>
+    <template #content>
+      <HeaderText :header-title="t('views.start-screen.header-title')"
+        :header-description="t('views.start-screen.header-description')" />
+    </template>
+  </OnBoardingLayout>
 </template>
