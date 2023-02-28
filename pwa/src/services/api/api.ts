c9c49071732e6aa9f1bba23d54af6d293a89f9b3
pdrzewicki <125042105+pdrzewicki@users.noreply.github.com>
2023-02-28 13:14:52 +0100

    PRED-38 Prepare API Service to connect with backend (Graphql) (#23)
    * (feat) Create service for connect to backend api
    
    * (feat) Create service api for connect to backend

---
+++ pwa/src/services/api/api.ts
@@ -0,0 +1,53 @@
+import axios from 'axios'
+
+type HeadersType = {
+  'content-type': string
+}
+
+type ParamsType<T> = {
+  readonly [K in keyof T]: T[K]
+}
+
+interface ApiService {
+  request<T, K>(query: string, params?: ParamsType<T>): Promise<K | undefined>
+}
+
+/**
+ * Main class of api service.
+ * @implements {ApiService}
+ */
+class ApiService implements ApiService {
+  /**
+   * @var {HeadersType}
+   */
+  #headers: HeadersType = {
+    'content-type': 'application/json',
+  }
+
+  /**
+   * Main method to send reuqest for graphql.
+   * @param {string} query
+   * @param {ParamsType<T>, undefined}params
+   * @returns {Promise<K | undefined>}
+   */
+  public async request<T, K>(
+    query: string,
+    params?: ParamsType<T>
+  ): Promise<K | undefined> {
+
+
+    return (
+      await axios({
+        url: import.meta.env.VITE_API_ENDPOINT,
+        method: 'post',
+        headers: this.#headers,
+        data: {
+          query,
+          variables: params,
+        },
+      })
+    ).data
+  }
+}
+
+export default new ApiService()
