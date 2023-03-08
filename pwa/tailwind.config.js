98b274f4d1cdd15dddb87d005dd4d0b98e5759ce
pdrzewicki <pdrzewicki@divante.com>
2023-03-08 11:49:30 +0100

    (fix) Fix options talwind

--- pwa/tailwind.config.js
+++ pwa/tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
-  darkMode: false,
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
   theme: {
     extend: {
       width: {
