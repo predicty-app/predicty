fb56fb9785373870b7deabd5cbd34e703c91977d
Jacek Kobus <kobus.jacek@gmail.com>
2023-03-13 22:31:29 +0100

    PRED-66 Implement async processing of  a uploaded file

--- api/src/Message/Command/ImportDataFromFacebookCsvFile.php
+++ api/src/Message/Command/ImportDataFromFacebookCsvFile.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Message\Command;
 
-class ImportDataFromFacebookCsvFile
+use App\Message\AsyncMessage;
+
+class ImportDataFromFacebookCsvFile implements AsyncMessage
 {
     public function __construct(public int $importId, public string $filename)
     {
